b67154e15a8a21112d0b32b6a3e751ea1b413d3b
hcwwadmin <48257922+hcwwadmin@users.noreply.github.com>
2020-01-17 08:15:30 +0200

    Update getGeo.sh to New Maxmind License
    Please update getGeo.sh to new maxmind license registrations

--- Docker/graylog/getGeo.sh
+++ Docker/graylog/getGeo.sh
@@ -1,2 +1,2 @@
-curl --output /etc/graylog/server/mm.tar.gz https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
+curl --output /etc/graylog/server/mm.tar.gz https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=YOUR_LICENSE_KEY&suffix=tar.gz
 tar zxvf /etc/graylog/server/mm.tar.gz -C /etc/graylog/server/ --strip-components=1
